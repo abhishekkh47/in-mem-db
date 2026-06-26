ab1d00b05feb2ed9e379bd0e07cecbd0b25aa974
abhishekkhmind <abhishek.khandelwal@mindinventory.com>
2026-06-26 17:30:36 +0530

    add help command

--- src/executor/command-executor.ts
+++ src/executor/command-executor.ts
@@ -7,19 +7,22 @@ class CommandExecutor {
     execute(command: ParseCommand) {
         const { command: cmd, args } = command
         const [key, value] = args
-        if (!key) {
-            return 'ERROR: Missing key'
-        }
         switch (cmd.toUpperCase()) {
             case 'SET':
-                if (!value) {
-                    return 'ERROR: Missing value'
+                if (key === undefined || value === undefined) {
+                    return 'ERROR: Missing key or value'
                 }
                 this.db.set(key, value)
                 return 'OK'
             case 'GET':
+                if (key === undefined) {
+                    return 'ERROR: Missing key'
+                }
                 return this.db.get(key)
             case 'DELETE':
+                if (key === undefined) {
+                    return 'ERROR: Missing key'
+                }
                 this.db.delete(key)
                 return 'OK'
             case 'CLEAR':
@@ -33,6 +36,18 @@ class CommandExecutor {
                 return this.db.has(key)
             case 'SIZE':
                 return this.db.size()
+            case 'HELP':
+                return ['Available commands:',
+                'SET <key> <value>',
+                'GET <key>',
+                'DELETE <key>',
+                'CLEAR',
+                'KEYS',
+                'VALUES',
+                'HAS <key>',
+                'SIZE',
+                'HELP']
+                .join('\n')
             default:
                 return `Unknown command: ${cmd}`
         }
